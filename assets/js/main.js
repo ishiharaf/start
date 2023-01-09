f5d41aed21dd52f1e78ac6edc9014cba7805cf18
Ishihara Felipe <k.hishihara@gmail.com>
2023-01-10 05:41:31 +0900

    Add charts

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,10 +34,34 @@ const updateCurrency = async (base, target) => {
 	}
 }
 
+const updateChart = async (base, target) => {
+	const data = await getCurrency(base, target)
+	const dates = [data.date]
+	const weeklyData = {}
+	for (let i = 0; i < 7; i++) {
+		const previousData = await getCurrency(base, target, dates[i])
+		weeklyData[dates[i]] = previousData[target]
+		dates.push(getPreviousDate(dates[i]))
+	}
+
+	const points = []
+	const prices = Object.values(weeklyData)
+	const ordered = [...prices].sort((a, b) => a - b)
+	for (let i = 0; i < 7; i++) {
+		const index = ordered.indexOf(prices[i])
+		const width = (i * 50 - 5) < 0 ? 5 : (i * 50 - 5)
+		const height = 100 - (index * 15 + 5)
+		points.push(`${width},${height}`)
+	}
+
+	const card = document.querySelector(`#${target}-card`)
+	card.style.backgroundImage = `url('data:image/svg+xml, <svg xmlns="http://www.w3.org/2000/svg" width="50" height="15" viewBox="0 0 300 100"><polyline fill="none" stroke="%230074d9" stroke-width="10" points="${points.join(" ")}"/></svg>')`
+}
+
 window.addEventListener("load", () => {
-	updateCurrency("usd", "jpy")
-	updateCurrency("usd", "brl")
-	updateCurrency("usd", "eur")
-	updateCurrency("usd", "gbp")
-	updateCurrency("usd", "cny")
+	updateCurrency("usd", "eur"), updateChart("usd", "eur")
+	updateCurrency("usd", "gbp"), updateChart("usd", "gbp")
+	updateCurrency("usd", "jpy"), updateChart("usd", "jpy")
+	updateCurrency("usd", "brl"), updateChart("usd", "brl")
+	updateCurrency("usd", "cny"), updateChart("usd", "cny")
 })
